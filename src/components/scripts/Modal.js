744731ee65089d9dfa8ead8b4c07b5795caa7abb
Artiom Shlyusberg <arte504@gmail.com>
2021-12-07 02:18:17 +0200

    Project 9: first commit

--- src/components/scripts/Modal.js
+++ src/components/scripts/Modal.js
@@ -1,6 +1,7 @@
 export default class Modal {
   constructor(modalSelector) {
-    this._modalElement = document.querySelector(modalSelector);
+    this._modalSelector = modalSelector;
+    this._checkForEscClose = this._checkForEscClose.bind(this);
   }
   // --- Close modal on ESC method --- //
   _checkForEscClose = (event) => {
@@ -10,24 +11,24 @@ export default class Modal {
   }
   // --- Open modal method --- // 
   open = () => {
-    this._modalElement.classList.add("modal_visible");
+    this._modalSelector.classList.add("modal_visible");
     document.addEventListener("keyup", this._checkForEscClose);
   }
   // --- Close modal method --- //
   close = () => {
-    this._modalElement.classList.remove("modal_visible");
+    this._modalSelector.classList.remove("modal_visible");
     document.removeEventListener("keyup", this._checkForEscClose);
   }
 
   setEventListeners() {
     // --- Close modal on close('X') button --- //
-    this._modalElement
+    this._modalSelector
     .querySelector(".modal__close-button")
     .addEventListener("click", (event) => {
       this.close(event);
     });
     // Close modal on overlay(out of the modal) click --- ///
-    this._modalElement.addEventListener("click", (event) => {
+    this._modalSelector.addEventListener("click", (event) => {
       if (event.target.classList.contains("modal_visible")) {
         this.close(event);
       }
